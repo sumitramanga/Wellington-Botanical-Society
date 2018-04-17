3a215fe078d33f72d89875fbbc1595b369c3890f
sumitra manga <845869602@bun2-206-12.local>
2018-04-17 18:50:40 +1200

    Finished Site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 
 $(document).ready(function(){
   $(".btnMenu").click(function(){
-    console.log("menu is clicked")
+    console.log("menu is clicked");
     $(".menu").toggle();
+  });
+  $(".btnMenu").click(function(){
+    console.log("menu color is clicked");
+    $(".btnMenu").toggleClass("btnMenuClicked");
   });
 });
